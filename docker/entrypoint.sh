de00ec0a3f32fd413c66ac2d4b20aaef2144a08a
Glenn Y. Rolland <glenux@glenux.net>
2021-11-30 15:16:01 +0100

    fix: handle user UID/GID

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,12 +9,12 @@ echo "Arguments: $ARGS"
 EXT_UID=${EXT_UID:-999}
 EXT_GID=${EXT_GID:-999}
 
-if ! grep "^[^:]*:[^:]*:$EXT_UID:" /etc/group ; then
+if ! grep -q "^[^:]*:[^:]*:$EXT_UID:" /etc/group ; then
 	groupadd -g "$EXT_GID" appuser
 fi
 
-if ! grep "^[^:]*:[^:]*:$EXT_UID:" /etc/passwd ; then
+if ! grep -q "^[^:]*:[^:]*:$EXT_UID:" /etc/passwd ; then
 	useradd -r -u "$EXT_UID" -g appuser appuser
 fi
 
-exec gosu appuser make "$@"
+exec gosu "$EXT_UID:$EXT_GID" make "$@"
