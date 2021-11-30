4131813fa50c46b1da78f06cd53c0fbdcc501e23
Glenn Y. Rolland <glenux@glenux.net>
2021-11-30 14:02:48 +0100

    docker: switch to non-root user

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+set -u
+set -e
+
 ARGS="$*"
 echo "Arguments: $ARGS"
 
-exec make "$@"
+EXT_UID=${EXT_UID:-999}
+EXT_GID=${EXT_GID:-999}
+
+groupadd -g "$EXT_GID" appuser
+useradd -r -u "$EXT_UID" -g appuser appuser
+
+exec gosu appuser make "$@"
